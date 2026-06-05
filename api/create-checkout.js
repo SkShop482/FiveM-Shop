a7295421703043690af0385cf2388ba7e3fc394d
SkShop482 <vapoxsk@gmail.com>
2026-06-05 13:26:47 +0200

    create-checkout.js

--- api/create-checkout.js
+++ api/create-checkout.js
@@ -1,6 +1,6 @@
-import Stripe from 'stripe';
+const Stripe = require('stripe');
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -45,4 +45,4 @@ export default async function handler(req, res) {
     console.error('Erreur Stripe:', err.message);
     return res.status(500).json({ error: err.message });
   }
-}
+};
